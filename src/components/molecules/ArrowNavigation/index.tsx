be682f45bdd310245b70e07a860a4833ac377b9e
Edafe Oghenefego <oghenefego@Edafes-MacBook-Pro.local>
2022-02-09 10:02:16 +0100

    feature(response list): build component

---
+++ src/components/molecules/ArrowNavigation/index.tsx
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import { QuestionContext } from "../../../providers/context";
+import Icon from "../../atoms/Icon";
+import styles from "./arrowNavigation.module.css";
+
+interface IArrowNavigation {
+  moveUp: () => void;
+  moveDown: () => void;
+  checked: boolean | undefined;
+}
+
+const ArrowNavigation: React.FC<IArrowNavigation> = ({
+  moveDown,
+  moveUp,
+  checked,
+}) => {
+  const { state } = useContext(QuestionContext);
+  const isFirstQuestion = state.questions.indexOf(state.currentQuestion) === 0;
+  return (
+    <div className={styles.arrowNavigation}>
+      <span className={`${isFirstQuestion ? styles.disabled : ""}`}>
+        <Icon onClick={moveDown} disabled={isFirstQuestion} rotate={true} />
+      </span>
+      <span className={`${!checked ? styles.disabled : ""}`}>
+        <Icon onClick={moveUp} disabled={!checked} />
+      </span>
+    </div>
+  );
+};
+
+export default ArrowNavigation;

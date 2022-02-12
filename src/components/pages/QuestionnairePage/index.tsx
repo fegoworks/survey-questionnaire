c58b137fc2f8c7eafb294bc4f484305cb2321a21
Edafe Oghenefego <oghenefego@Edafes-MacBook-Pro.local>
2022-02-12 20:38:06 +0100

    feature(questionnaire page): build component

---
+++ src/components/pages/QuestionnairePage/index.tsx
@@ -0,0 +1,67 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import useQuestionnaire from "../../../hooks/useQuestionnaire";
+import { QuestionContext } from "../../../providers/context";
+import Content from "../../complexes/Content";
+import Footer from "../../complexes/Footer";
+import Header from "../../complexes/Header";
+import QuestionnaireIntro from "../QuestionnaireIntro";
+import styles from "./questionnaire.module.css";
+
+const Questionnaire: React.FC = () => {
+  const [animate, setAnimate] = useState<boolean>(false);
+  const [showIntro, setShowIntro] = useState<boolean>(true);
+  const { questionnaireId } = useParams();
+  const { questions, name, description } = useQuestionnaire(questionnaireId);
+  const {
+    state,
+    markAsSelected,
+    setNextQuestion,
+    setPrevQuestion,
+    loadQuestionnaire,
+  } = useContext(QuestionContext);
+
+  useEffect(() => {
+    if (questions) {
+      loadQuestionnaire(questions);
+    }
+  }, [questions]);
+
+  const { currentQuestion } = state;
+
+  const selectChoice = (event: React.MouseEvent<HTMLDivElement>) => {
+    const textContent = event.currentTarget.id;
+
+    markAsSelected(textContent);
+    setNextQuestion(textContent);
+    setAnimate(!animate);
+  };
+
+  return (
+    <div className={styles.questionnaire}>
+      {showIntro ? (
+        <QuestionnaireIntro
+          questionnaireName={name}
+          questionnaireDescription={description}
+          handleClick={() => setShowIntro(!showIntro)}
+        />
+      ) : (
+        <>
+          <Header text="Survey Questions" />
+          <Content
+            question={currentQuestion}
+            handleClick={selectChoice}
+            animate={animate}
+          />
+          <Footer
+            moveDown={() => setPrevQuestion()}
+            moveUp={() => setNextQuestion()}
+            checked={currentQuestion?.checked}
+          />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Questionnaire;

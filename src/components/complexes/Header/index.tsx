e7950e8fce2c5950d1c2b8d9a8daaf2532a9214d
Edafe Oghenefego <oghenefego@Edafes-MacBook-Pro.local>
2022-02-12 20:33:06 +0100

    feature(header): build component

--- src/components/complexes/Header/index.tsx
+++ src/components/complexes/Header/index.tsx
@@ -1,9 +1,21 @@
+import styles from "./header.module.css";
+import { Link } from "react-router-dom";
 interface IHeader {
   text: string;
+  isGrey?: boolean;
 }
 
-const Header: React.FC<IHeader> = ({ text }) => {
-  return <header className="header">{text}</header>;
+const Header: React.FC<IHeader> = ({ text, isGrey }) => {
+  return (
+    <header className={styles.header}>
+      <Link to="/" className={styles.menuBar}>
+        <div className={`${styles.line} ${isGrey ? styles.isGrey : ""}`}></div>
+        <div className={`${styles.line} ${isGrey ? styles.isGrey : ""}`}></div>
+        <div className={`${styles.line} ${isGrey ? styles.isGrey : ""}`}></div>
+      </Link>
+      <div>{text}</div>
+    </header>
+  );
 };
 
 export default Header;

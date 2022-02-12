9c16047605d1784cfcd6ad2e6c6afe45273c6376
Edafe Oghenefego <oghenefego@Edafes-MacBook-Pro.local>
2022-02-12 20:33:27 +0100

    feature(header): refactor component

--- src/components/molecules/ArrowNavigation/index.tsx
+++ src/components/molecules/ArrowNavigation/index.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import useCounter from "../../../hooks/useCounter";
 import { QuestionContext } from "../../../providers/context";
 import Icon from "../../atoms/Icon";
 import styles from "./arrowNavigation.module.css";
@@ -15,14 +16,28 @@ const ArrowNavigation: React.FC<IArrowNavigation> = ({
   checked,
 }) => {
   const { state } = useContext(QuestionContext);
+  const { questionNumber, setCounter } = useCounter(state.currentQuestion);
   const isFirstQuestion = state.questions.indexOf(state.currentQuestion) === 0;
   return (
     <div className={styles.arrowNavigation}>
       <span className={`${isFirstQuestion ? styles.disabled : ""}`}>
-        <Icon onClick={moveDown} disabled={isFirstQuestion} rotate={true} />
+        <Icon
+          onClick={() => {
+            moveDown();
+            setCounter(questionNumber - 1);
+          }}
+          disabled={isFirstQuestion}
+          rotate={true}
+        />
       </span>
       <span className={`${!checked ? styles.disabled : ""}`}>
-        <Icon onClick={moveUp} disabled={!checked} />
+        <Icon
+          onClick={() => {
+            moveUp();
+            setCounter(questionNumber + 1);
+          }}
+          disabled={!checked}
+        />
       </span>
     </div>
   );
